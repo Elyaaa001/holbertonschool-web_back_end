ed9ebc1c8ef76075e03b62364cd0c44d12f141b0
Elyaaa001 <gabibovelina75@gmail.com>
2024-11-25 12:00:56 +0400

    task done

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+    const arrBuff = new ArrayBuffer(length);
+    const int8view = new Int8Array(arrBuff);
+    if (position > length) {
+      throw Error('Position outside range');
+    } else {
+      int8view[position] = value;
+    }
+    return new DataView(arrBuff);
+  }
